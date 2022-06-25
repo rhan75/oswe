1b2ec4e46f3db6dd91523189c41570cf27415a2b
rhan75 <rhan75@gmail.com>
2022-06-25 12:57:10 -0700

    web listener 1st version

---
+++ weblisterner.py
@@ -0,0 +1,19 @@
+from ast import For
+import requests
+from colorama import Fore, Back, Style
+from sympy import false
+
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+def format_text(title, item):
+    cr = '\r\n'
+    section_break = cr + '*' * 20 + cr
+    item = str(item)
+    text = Style.BRIGHT + Fore.RED + title + Fore.BLACK + section_break + item + section_break
+    return text
+
+r = requests.get('https://www.seattle.gov', verify=false)
+print(format_text('r.status_code is: ', r.status_code))
+print(format_text('r.headers is: ',r.headers))
+print(format_text('r.cookies is: ',r.cookies))
+print(format_text('r.text is: ',r.text))
