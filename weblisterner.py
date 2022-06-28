eeec1c47341f3c3239fc9247e02a2f6abcc39ec3
rhan75 <rhan75@gmail.com>
2022-06-27 22:48:27 -0700

    Code clean up:better naming / old code removal

--- weblisterner.py
+++ weblisterner.py
@@ -2,9 +2,10 @@ import requests
 from colorama import Fore, Back, Style
 import argparse
 
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+#Suppress Insecure Request Warning
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning) 
 
-#proxies = {'http':'http://127.0.0.1:8080', 'https':'http://127.0.0.1:8080'}
+burp_proxies = {'http':'http://127.0.0.1:8080', 'https':'http://127.0.0.1:8080'}
 
 def format_text(title, item):
     cr = '\r\n'
@@ -13,31 +14,31 @@ def format_text(title, item):
     text = Style.BRIGHT + Fore.RED + title + Fore.BLACK + section_break + item + section_break
     return text
 
-# r = requests.get('https://www.yahoo.com', verify=False, proxies=proxies)
-# print(format_text('r.status_code is: ', r.status_code))
-# print(format_text('r.headers is: ',r.headers))
-# print(format_text('r.cookies is: ',r.cookies))
-# print(format_text('r.text is: ',r.text))
 
 def main():
-
-    r=requests.get(args.url, verify=False)
+    if args.proxy.lower() == 'yes':
+        r=requests.get(args.url, verify=False, proxies=burp_proxies)
+    elif args.proxy.lower() == 'no':
+        r=requests.get(args.url, verify=False)
+    else:
+        print('Wrong proxy argument used. Yes to use Burpsuite or No to send it directly.')
+        exit()
     
     if not args.section:
-        print(format_text('r.status_code is: ', r.status_code))
-        print(format_text('r.headers is: ',r.headers))
-        print(format_text('r.cookies is: ',r.cookies))
-        print(format_text('r.text is: ',r.text))
+        print(format_text('Status Code: ', r.status_code))
+        print(format_text('Headers: ',r.headers))
+        print(format_text('Cookies: ',r.cookies))
+        print(format_text('Text: ',r.text))
     elif args.section == 'sc':
-        print(format_text('r.status_code is: ', r.status_code))
+        print(format_text('Status Code: ', r.status_code))
     elif args.section == 'h':
-        print(format_text('r.headers is: ',r.headers))
+        print(format_text('Headers: ',r.headers))
     elif args.section == 'c':
-        print(format_text('r.cookies is: ',r.cookies))
+        print(format_text('Cookies: ',r.cookies))
     elif args.section == 'text':
-        print(format_text('r.text is: ',r.text))
+        print(format_text('Text: ',r.text))
     else:
-        print('Wrong section argument. Please use sc for Status Code, h for headers, c for cookies, text for the main. Or no section argument will print out all sections.')
+        print('Incorrect argument for --section. Please use \'sc\' for Status Code, \'h \' for headers, \'c\' for cookies, \'text\' for the main. Or no section argument will print out all sections.')
 
 
 
@@ -46,5 +47,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='OSWE Web Listerner for Burpsuite')
     parser.add_argument('--url', metavar='path', required=True, help='URL to test') #Need to check for correct URL
     parser.add_argument('--section', metavar='path', required=False, help='sc for Status Code, h for headers, c for cookies, text for the main') #Need to check for correct arguments
+    parser.add_argument('--proxy', metavar='path', required=True, help='yes for Burpsuite, no for no proxy.')
     args = parser.parse_args()
     main()
