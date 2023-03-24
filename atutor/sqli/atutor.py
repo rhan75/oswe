e128c9422cdcb4ef443f0218abf318aa6e19d019
rhan75 <rhan75@gmail.com>
2023-03-23 21:49:19 -0700

    Clean up

--- atutor/sqli/atutor.py
+++ atutor/sqli/atutor.py
@@ -1,26 +1,25 @@
 import re
 import requests
 import argparse
-from bs4 import BeautifulSoup
+
 
 def searchFriends_sqli(ip, inj_str):
     target = f'http://{ip}/ATutor/mods/_standard/social/index_public.php?q={inj_str}'
-    r = requests.get(target)
-    s = BeautifulSoup(r.text, 'lxml')
+    response = requests.get(target)
     result = {}
-    error = re.search("Invalid argument", s.text)
+    error = re.search("Invalid argument", response.text)
     if error:
-        result['injection'] = 'Possible'
+        result['injectible'] = True
     else:
-        result['injection'] = 'Not injectible'
-    result['header'] = r.headers
-    result['text'] = s.text
+        result['injection'] = False
+    result['header'] = response.headers
+    result['text'] = response.text
     return result
 
 def main():
     parser = argparse.ArgumentParser(description='Check for SQL Injection.')
-    parser.add_argument('-i', '--ip', type=str, help='ip address or FQDN')
-    parser.add_argument('-s', '--string', type=str, help='injection string')
+    parser.add_argument('-i', '--ip', type=str, help='ip address or FQDN', required=True)
+    parser.add_argument('-s', '--string', type=str, help='injection string', required=True)
 
     args = parser.parse_args()
     result = searchFriends_sqli(args.ip, args.string)
